4835928e362c8c760374cf95c49d296040f9015e
Orlando Mason <orlandomason11@gmail.com>
2018-06-26 17:57:50 +0200

    generate_map.js: the actual file.

--- generate_map.js
+++ generate_map.js
@@ -1,11 +1,34 @@
 
 var asteroids = [];
 
+// In metres
+var asteroid_max_size = 100;
+var asteroids_min_size = 50;
+
+var chance_of_max_size = 0.01;
+var number_of_asteroids = 100;
+
 function generateMap(width, height, asteroid_density) {
 
-	for (var i = 0; i < ; i++) {
-		asteroids[i] = new asteroid("c-type");
+	var asteroid_count = 0;
+
+	for (var i = asteroid_max_size; i > asteroid_min_size; i--) {
+
+		// Average Number of asteroids at asteroid size i = Math.round( (max_asteroid_size)^3 * asteroid_size^-3)
+
+		var average_number = Math.pow(asteroid_max_size, 3) * Math.pow(asteroid_size, -3);
+
+
+		}
+
+		for (var j = 0; j < noaaasi; j++) {
+
+			asteroids.push(new asteroid("c-type", ));
+			asteroid_count++;
+
+			if (asteroid_count >= number_of_asteroids) {
+				// break for loop
+			}
+		}
 	}
 }
-
-1.5 million asteroids in the main asteroid belt that are larger than 1 km 
