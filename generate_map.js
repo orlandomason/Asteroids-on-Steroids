dea8c9666f41e2baf84e82acd62e0dbbc678c57f
Orlando Mason <orlandomason11@gmail.com>
2018-06-26 17:49:26 +0200

    generate_map.js

---
+++ generate_map.js
@@ -0,0 +1,11 @@
+
+var asteroids = [];
+
+function generateMap(width, height, asteroid_density) {
+
+	for (var i = 0; i < ; i++) {
+		asteroids[i] = new asteroid("c-type");
+	}
+}
+
+1.5 million asteroids in the main asteroid belt that are larger than 1 km 
